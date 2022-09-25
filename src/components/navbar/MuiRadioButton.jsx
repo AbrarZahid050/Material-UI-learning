083704052612e27cb76baea3a1b780b42c9a26e9
AbrarZahid050 <abrar.zahid050@gmail.com>
2022-09-26 00:34:40 +0500

    update-1

--- src/components/navbar/MuiRadioButton.jsx
+++ src/components/navbar/MuiRadioButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Box,
   Container,
@@ -6,24 +7,47 @@ import {
   FormControlLabel,
   RadioGroup,
   Radio,
+  FormHelperText,
 } from "@mui/material";
 
 const MuiRadioButton = () => {
+  const [value, setValue] = useState("");
+
+  const selectBtnHandler = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <Container>
       <Box margin={3}>
-        <FormControl>
+        <FormControl error={!value ? true : false} required>
           <FormLabel id="job-experience-group-label">
             Years of experience
           </FormLabel>
           <RadioGroup
             name="job-experience-group"
             aria-labelledby="job-experience-group-label"
+            value={value}
+            onChange={selectBtnHandler}
+            row
           >
-            <FormControlLabel control={<Radio />} label="0-2" value="0-2" />
-            <FormControlLabel control={<Radio />} label="3-5" value="3-5" />
-            <FormControlLabel control={<Radio />} label="6-10" value="6-10" />
+            <FormControlLabel
+              control={<Radio size="small" />}
+              label="0-2"
+              value="0-2"
+            />
+            <FormControlLabel
+              control={<Radio size="small" />}
+              label="3-5"
+              value="3-5"
+            />
+            <FormControlLabel
+              control={<Radio size="small" />}
+              label="6-10"
+              value="6-10"
+            />
           </RadioGroup>
+          {!value ? <FormHelperText>Please select one </FormHelperText> : null}
         </FormControl>
       </Box>
     </Container>
